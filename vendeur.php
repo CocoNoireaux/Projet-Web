2b990e53522ad4d2cef8a97ea337b8bb0f3a15a7
mathilde0518 <36438915+mathilde0518@users.noreply.github.com>
2019-04-30 14:24:19 +0200

    Add files via upload

--- vendeur.php
+++ vendeur.php
@@ -11,20 +11,33 @@ $database="eceamazon";
 //connecter l'utilisateur dans BDD
 $db_handle=mysqli_connect(DB_SERVER,DB_USER,DB_PASS);
 $db_found=mysqli_select_db($db_handle,$database);
-if (!$db_found)
+
+
+$pseudo = isset($_POST["pseudo"])? $_POST["pseudo"] : "";
+$email = isset($_POST["email"])? $_POST["email"] : "";
+$photo = isset($_POST["photo"])? $_POST["photo"] : "";
+$photofond = isset($_POST["photofond"])? $_POST["photofond"] : "";
+if($_POST["bouton"])
 {
-    
-    echo "la connexion au serveur n'est pas établie";
+    if ($db_found)
+    {
+        $sql="INSERT INTO vendeur(pseudo, email, photo, photofond) VALUES ('$pseudo','$email','$photo','$photofond')";
+        
+$result = mysqli_query($db_handle, $sql);
+	echo "L'ajout a bien été effectué";
+
+    }
+
+else 
+{
+    echo "data base not found";
 }
 
 
-$pseudo=$_POST['pseudo'];
-$email=$_POST['email'];
-$photo=$_POST['photo'];
-$photofond=$_POST['photofond'];
+	mysqli_close($db_handle);
 
-$sql="INSERT INTO vendeur (pseudo, email, photo, photofond) VALUES ($pseudo,$email,$photo,$photofond)";
 
-//fermer la connection
-mysqli_close($db_handle);
+}
+
 ?>
+
